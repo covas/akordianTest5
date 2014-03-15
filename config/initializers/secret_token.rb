d4a90b7fdd30eeea345ff94f6f56ae4ab200cf9f
Camilo <cbarraza11@gmail.com>
2014-03-15 03:39:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AkordianTest5::Application.config.secret_key_base = '85be81d61bbcaa2e6eb39f3c6e6c051b6b7beb8d42bb48da64d1e0567bc065282b073ef927d1e9f88f6f8ad9e6d7941f0abb7886bf24fb6794b07b35fe7937f1'
