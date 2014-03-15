3b0c025cf1b39d7b7b2665fed6102972b61c1023
Camilo <cbarraza11@gmail.com>
2014-03-15 03:53:19 -0500

    le quite las bases de datos

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 AkordianTest5::Application.initialize!
 
+config.frameworks -= [ :active_record, :active_resource, :action_mailer]
